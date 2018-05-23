3eaec98fb96dd26a32115662a46609434bea27e4
Muqsit Nawaz <muqsitnawaz@gmail.com>
2018-05-22 21:41:58 -0700

    Add get/put ops

--- edge.py
+++ edge.py
@@ -6,6 +6,46 @@ import json
 import pickle
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
+from pathlib import Path
+
+ss_path = './data/'+sys.argv[1]
+
+def convert(data):
+    if isinstance(data, bytes):  return data.decode('ascii')
+    if isinstance(data, dict):   return dict(map(convert, data.items()))
+    if isinstance(data, tuple):  return map(convert, data)
+    if isinstance(data, list):   return convert(data[0])
+    return data
+
+def write_to_ss(fields):
+    table_path = Path(ss_path+'/'+fields['user_id'])
+
+    if table_path.is_file():
+        user_data = {}
+
+        with open(table_path, 'rb') as file:
+            user_data = pickle.loads(file.read())
+            print(user_data)
+
+        with open(table_path, 'wb') as file:
+            user_data[fields['key']] = fields['value']
+            file.write(pickle.dumps(user_data))
+        
+        print('Done')
+
+def read_from_ss(fields):
+    table_path = Path(ss_path+'/'+fields['user_id'])
+
+    if table_path.is_file():
+        user_data = {}
+
+        with open(table_path, 'rb') as file:
+            user_data = pickle.loads(file.read())
+            if fields['key'] in user_data.keys():
+                return user_data[fields['key']]
+            else:
+                return 'Bad key'
+
 
 def connect_to_dc(sock, port):
     sock.connect((socket.gethostname(), port))
@@ -25,9 +65,23 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
-        fields = parse_qs(self.rfile.read(content_length)) 
-        print(fields)
+        fields = parse_qs(self.rfile.read(content_length))
+        fields = convert(fields)
+
+        if 'user_id' in fields.keys():
+            if 'value' in fields.keys():
+                write_to_ss(fields)
+                self.send_resp("Success")
+            else:
+                value = read_from_ss(fields)
+                self.send_resp(value)
+
+        else:
+            self.send_resp("Bad user_id")
         return
+
+    def send_resp(self, message):
+        self.wfile.write(bytes(message, "utf8"))
 
 def run_http():
   server_address = ('127.0.0.1', 9001)
